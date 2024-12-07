c34e3a7a423f9595fa7243764e8dc55bb592f7b0
cds <due179@gmail.com>
2024-12-07 23:32:05 +0100

    Add inital setup for 2024

---
+++ 2024/Program.cs
@@ -0,0 +1,22 @@
+﻿Action[] solutions =
+[
+    Day01.Run, Day02.Run, Day03.Run
+];
+
+foreach (int index in ProblemIndices())
+{
+    solutions[index]();
+}
+
+IEnumerable<int> ProblemIndices()
+{
+    string prompt = $"{Environment.NewLine}Enter the day number [1-{solutions.Length}] (ENTER to quit): ";
+    Console.Write(prompt);
+    while (true)
+    {
+        string input = Console.ReadLine() ?? string.Empty;
+        if (string.IsNullOrEmpty(input)) yield break;
+        if (int.TryParse(input, out int number) && number >= 1 && number <= solutions.Length) yield return number - 1;
+        Console.Write(prompt);
+    }
+}
